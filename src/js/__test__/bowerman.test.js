f9ca2d72f17ed67987f0cdcc1fbe73161e1c2fe5
Kiyas <kiiyas@yandex.ru>
2021-08-30 13:38:09 +0500

    covering completed

--- src/js/__test__/bowerman.test.js
+++ src/js/__test__/bowerman.test.js
@@ -11,3 +11,15 @@ test('Must correctly create Bowerman character object', () => {
   };
   expect(new Bowerman('Test', 'Bowerman')).toEqual(expected);
 });
+
+test('Must correctly create Bowerman character object when in arguments there is only name', () => {
+  const expected = {
+    name: 'Test',
+    type: 'Bowerman',
+    health: 100,
+    level: 1,
+    attack: 25,
+    defens: 25,
+  };
+  expect(new Bowerman('Test')).toEqual(expected);
+});
